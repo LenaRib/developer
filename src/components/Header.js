79556a82b67a70a8582c1bcefeb2efe6860e6365
 Elena Poddubnaia <lenarib@Elenas-MacBook-Pro.local>
2022-01-10 10:42:38 +0100

    init

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div>
+      <ul>
+        <li>GitHub</li>
+        <li>CodePin</li>
+        <li>Resume</li>
+      </ul>
+      </div>
+    )
+  }
+}
+
+//  <h1>Elena Poddubnaia</h1>
+      //  <p>Junior Frontend Developer, living in Gdansk, Poland. <br /> 
+      //  Available for new working opportunities.</p>
